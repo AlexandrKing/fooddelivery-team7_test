45d6cccc68a100870709e4052a98177c0f57e552
dsbobryakov <dmitrybobryakov@gmail.com>
2025-11-13 19:30:42 +0300

    Hotfix: change jdk version 21 -> 17

--- src/main/java/com/team7/restaurant/model/Dish.java
+++ src/main/java/com/team7/restaurant/model/Dish.java
@@ -12,19 +12,8 @@ public class Dish {
   private Long id;
   private String name;
   private String description;
-  private BigDecimal price;
+  private double price;
   private String category;
   private Boolean available;
   private Long restaurantId;
-
-  public Dish(Long id, String name, String description, BigDecimal price,
-              String category, Long restaurantId) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.category = category;
-    this.restaurantId = restaurantId;
-    this.available = true;
-  }
 }
