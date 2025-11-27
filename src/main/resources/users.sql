c6820f2a3fc8b747d345283c5de72fea852fc152
Azat Aminov <aminovazatrinatovich@gmail.com>
2025-11-27 19:11:30 +0300

    добавил data

--- src/main/resources/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    phone VARCHAR(20),
-    password VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL
-);
